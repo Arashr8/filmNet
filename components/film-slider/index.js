2caa26cfb823276f7cfef0ac7f5a7487c6e69f42
Arashr8 <arash1ra@hotmail.com>
2022-01-07 12:04:57 +0100

    hover for card films

--- components/film-slider/index.js
+++ components/film-slider/index.js
@@ -14,73 +14,73 @@ const FilmSlider = () => {
       >
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film1.jpg"} alt="image 1"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film2.jpg"} alt="image 2"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film3.jpg"} alt="image 3"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film4.jpg"} alt="image 4"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film5.jpg"} alt="image 5"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film6.jpg"} alt="image 6"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film1.jpg"} alt="image 7"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film2.jpg"} alt="image 8"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film3.jpg"} alt="image 8"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film4.jpg"} alt="image 9"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-            <Card>
+            <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film5.jpg"} alt="image 10"/>
               </Card>
           </SwiperSlide>
 
           <SwiperSlide>
-              <Card>
+              <Card className={styles.card}>
                   <Card.Img src={"/assets/films/film6.jpg"} alt="image 11"/>
               </Card>
           </SwiperSlide>
